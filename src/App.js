9f7dadbad7785288be6f658361e2b1de0fb684d2
John Taggart <john@MacBook-Pro.connect>
2020-06-20 15:54:30 +0100

    character components showing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import GameContainer from './containers/GameContainer';
 import './App.css';
 
 function App() {
   return (
     <div>
-    <GameContainter/>
+    <GameContainer />
     <h1>App start</h1>
     </div>
   );
